13c27607ba5623733088d45394bfb8c1b7a5eccd
Muhammad Yousaf <muhammad.yousaf6239@gmail.com>
2026-04-20 17:54:51 +0500

    Users view, template and form added

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("users/", views.users_list, name="users_list"),
+    path("users/delete/<int:user_id>/", views.delete_user, name="delete_user"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password
+from django.views.decorators.http import require_POST
 from .models import User
 
 
 def users_list(request):
-	users = User.objects.all()
-	return render(request, "myapp/users.html", {"users": users})
+    if request.method == "POST":
+        name = request.POST.get("name", "").strip()
+        email = request.POST.get("email", "").strip()
+        password = request.POST.get("password", "")
+
+        if name and email and password:
+            User.objects.create(
+                name=name,
+                email=email,
+                password=make_password(password),
+            )
+        return redirect("/users/")
+
+    users = User.objects.all()
+    return render(request, "myapp/users.html", {"users": users})
+
+
+@require_POST
+def delete_user(request, user_id):
+    User.objects.filter(id=user_id).delete()
+    return redirect("users_list")
